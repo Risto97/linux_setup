0fc273f8ae10d733a5dc4fa3252003705ab20a1f
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2020-07-19 17:06:25 +0200

    fix neovim nodejs install

--- configs/install.sh
+++ configs/install.sh
@@ -64,5 +64,11 @@ chmod +x ~/bin/nvim.appimage
 mv ~/bin/nvim.appimage ~/bin/nvim
 rm -rf ~/.config/nvim
 ln -s $(pwd)/nvim ~/.config/nvim
-./coc.nvim.sh
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install node
+
 
