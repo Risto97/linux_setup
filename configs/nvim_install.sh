7e4bb6511dc195466a999ee07ba7415df20dfe0c
Risto Pejasinovic <risto@nvim4.cern.ch>
2022-06-12 09:04:10 +0000

    new nvim

--- configs/nvim_install.sh
+++ configs/nvim_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir -p ~/.local/bin
-wget --directory-prefix=$HOME/.local/bin https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
+wget --directory-prefix=$HOME/.local/bin https://github.com/neovim/neovim/releases/download/v0.7.0/nvim.appimage
 chmod +x ~/.local/bin/nvim.appimage
 mv ~/.local/bin/nvim.appimage ~/.local/bin/nvim
 rm -rf ~/.config/nvim
 ln -s $(pwd)/nvim ~/.config/nvim
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
